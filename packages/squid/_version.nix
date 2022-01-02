64cfb9e5e3bba0f7f5c3ef33ca372e54ea90e978
Serge Pokhodyaev <snp@z9.ru>
2022-01-02 09:36:16 +0300

    add squid 5.3

---
+++ packages/squid/_version.nix
@@ -0,0 +1,4 @@
+{
+  version = "5.3";
+  sha256 = "45178588df1311ded41ebadd632840c4d93a8d7f5f60e38e74acf2f1ae2f1715";
+}
---
+++ packages/squid/default.nix
@@ -0,0 +1,47 @@
+{ pkgs, }:
+let
+  inherit (import ./_version.nix) version sha256;
+in
+pkgs.stdenv.mkDerivation rec {
+  pname = "squid";
+  inherit version;
+
+  src = pkgs.fetchurl {
+    url = "http://www.squid-cache.org/Versions/v${pkgs.lib.versions.major version}/${pname}-${version}.tar.xz";
+    inherit sha256;
+  };
+
+  nativeBuildInputs = [ pkgs.pkg-config ];
+  buildInputs = [
+    pkgs.perl pkgs.db pkgs.openssl pkgs.libcap
+  ]; # pkgs.pam pkgs.expat pkgs.libxml2 pkgs.cyrus_sasl pkgs.openldap
+
+  configureFlags = [
+    "--disable-ipv6" # "--enable-ipv6"
+    "--disable-auto-locale"
+#    "--disable-strict-error-checking"
+    "--disable-arch-native"
+    "--disable-wccp"
+    "--disable-wccpv2"
+    "--disable-snmp"
+    "--enable-cachemgr-hostname=localhost"
+    "--disable-ident-lookups"
+    "--disable-auth"
+    "--with-openssl"
+    "--enable-ssl-crtd"
+    "--enable-storeio=ufs,aufs,diskd,rock"
+    "--enable-removal-policies=lru,heap"
+    "--enable-delay-pools"
+    "--disable-esi"
+    "--disable-htcp"
+#    "--enable-x-accelerator-vary"
+    "--enable-linux-netfilter"
+  ];
+
+  enableParallelBuilding = true;
+
+  meta = {
+    inherit (pkgs.squid.meta) description homepage license;
+    platforms = with pkgs.lib.platforms; linux;
+  };
+}

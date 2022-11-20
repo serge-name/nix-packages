347f17561a0aa6145eafe7d7e882eddf9d12fc9a
Serge Pokhodyaev <snp@z9.ru>
2022-11-20 14:24:12 +0200

    simplify (remove unused architectures)

--- flake.nix
+++ flake.nix
@@ -3,22 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
   let
+    system = "x86_64-linux";
+    pkgs = inputs.nixpkgs.legacyPackages.${system};
     myLib = import ./lib { inherit (nixpkgs) lib; };
-  in
-    (inputs.flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = inputs.nixpkgs.legacyPackages.${system};
-        in rec {
-          packages = inputs.flake-utils.lib.flattenTree (myLib.mkPackages ./packages pkgs);
-        }
-      )
-    ) // {
-      lib = import ./lib;
-    };
+  in {
+    packages.${system} = myLib.mkPackages ./packages pkgs;
+  };
 }

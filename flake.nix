2b05ba2c310bc999a38d055631a8115f695619cc
Serge Pokhodyaev <snp@z9.ru>
2022-11-27 14:15:03 +0200

    add 'nix run .#repl'

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
@@ -13,6 +14,7 @@
     unstable = inputs.unstable.legacyPackages.${system};
     myLib = import ./lib { inherit (stable) lib; };
   in {
+    apps.${system}.repl = import ./lib/repl.nix { inherit stable inputs; };
     packages.${system} = myLib.mkFlakeParts ./packages { inherit stable unstable; };
   };
 }
---
+++ lib/repl.nix
@@ -0,0 +1,17 @@
+# Also see https://github.com/NixOS/nix/issues/3803
+
+{ stable, inputs, ... }:
+let
+  inherit (inputs.flake-utils.lib) mkApp;
+  inherit (stable) writeShellScriptBin;
+in
+mkApp {
+  drv = writeShellScriptBin "repl" ''
+    confnix=$(mktemp)
+    flake_dir="$(git rev-parse --show-toplevel || pwd)"
+    cmd="{ fl = builtins.getFlake (toString $flake_dir); }"
+    echo $cmd >$confnix
+    trap "rm -f $confnix" EXIT
+    nix repl $confnix
+  '';
+}

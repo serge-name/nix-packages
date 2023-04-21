ac3eaf48bdced33318bdfaa02cfb9c77801ae3c3
Serge Pokhodyaev <snp@z9.ru>
2023-04-21 20:23:51 +0300

    bump [2]

--- packages/yt-dlp/_version.nix
+++ packages/yt-dlp/_version.nix
@@ -1,5 +1,5 @@
 {
   version = "master-20230419";
   rev = "8f0be90ecb3b8d862397177bb226f17b245ef933";
-  sha256 = "sha256-hOewfptCu8D1oTzyhvTaMBWXQKJ9Zux9nrxmfCRdZP0=";
+  sha256 = "sha256-TmJ/mJ5Eo/yK7/mPx6MBNqYU4F4byLJLyxlWceeYJFk=";
 }

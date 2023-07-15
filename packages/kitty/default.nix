ac2e3cfab504474b5825621576d5fb13bb66dfe2
Serge Pokhodyaev <snp@z9.ru>
2023-07-15 14:27:00 +0300

    upd kitty

--- packages/kitty/default.nix
+++ packages/kitty/default.nix
@@ -1,14 +1,22 @@
 { unstable, ... }:
 let
-  inherit (unstable) stdenv writeShellScript kitty glibcLocales mesa_drivers;
+  inherit (unstable) stdenv writeShellScript kitty glibcLocales mesa_drivers gnugrep;
+
+  grep = gnugrep + "/bin/grep";
+  mesa_lib = mesa_drivers + "/lib";
+  mesa_dri = mesa_lib + "/dri";
+
+  foo = varname: path: ''
+    if ! ${grep} -qF "${path}" <<< "''${${varname}:-}"; then
+      export ${varname}="${path}''${${varname}:+:}''${${varname}:-}"
+    fi
+  '';
 
   wrapper = writeShellScript "kitty-wrapper" ''
     export LOCALE_ARCHIVE=''${LOCALE_ARCHIVE:-'${glibcLocales}/lib/locale/locale-archive'}
+    ${foo "LD_LIBRARY_PATH" mesa_lib}
+    ${foo "LIBGL_DRIVERS_PATH" mesa_dri}
     exec -a "$0" "${kitty}/bin/kitty" "$@"
-  '';
-
-  script = writeShellScript "kitty-opengl-driver-path" ''
-    echo "${mesa_drivers}"
   '';
 
 in stdenv.mkDerivation {
@@ -20,6 +28,5 @@ in stdenv.mkDerivation {
     mkdir -p $out/bin
     find ${kitty} -mindepth 1 -maxdepth 1 -not -name bin |xargs -n1 ln -st $out
     ln -sT ${wrapper} $out/bin/kitty
-    ln -sT ${script} $out/bin/${script.name}
   '';
 }

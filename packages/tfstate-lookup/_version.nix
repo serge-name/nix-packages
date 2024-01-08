938eaa942182eb63314e73bc4a690c38455fd7ed
Serge Pokhodyaev <snp@z9.ru>
2024-01-08 11:10:18 +0200

    bump

--- packages/tfstate-lookup/_version.nix
+++ packages/tfstate-lookup/_version.nix
@@ -1,5 +1,5 @@
 {
-  version = "1.1.2";
-  sha256 = "sha256-gFim+lGkkPEFyT5F16WA3zJ6T9HSOBN9LJ2HyqonrRw=";
-  vendorHash = "sha256-biu1fVJB/tb0s3VXICOT9Z14MIJvdnbotHxjMtnGh1o=";
+  version = "1.1.6";
+  sha256 = "sha256-reY0a4yA4+6YUHB7YBC8p0x41wzBSBDdeDTLry0Z2SQ=";
+  vendorHash = "sha256-s+68rrW0JY7JIAB9ZkWUgwjP5ti8MjMPn5uJWqZ0wjs=";
 }
--- packages/yt-dlp/_version.nix
+++ packages/yt-dlp/_version.nix
@@ -1,5 +1,5 @@
 {
-  version = "master-20230812";
-  rev = "339c339fec095ff4141b20e6aa83629117fb26df";
-  sha256 = "sha256-qrTKVgaJ44tOKPJ1y/Lz5OXvtSSFhfG8ev1LdA8EdeE=";
+  version = "master-20240105";
+  rev = "b6951271ac014761c9c317b9cecd5e8e139cfa7c";
+  sha256 = "sha256-qOllCsvRRNPu5Jln0iiFv+BJsFrR/veseEnjo1cOYms=";
 }

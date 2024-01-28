09a0c1a4a922d0845738792bcd837b0ff3dc79aa
Serge Pokhodyaev <snp@z9.ru>
2024-01-28 13:17:30 +0200

    bump

--- packages/timoni/_version.nix
+++ packages/timoni/_version.nix
@@ -1,5 +1,5 @@
 {
-  version = "0.18.0";
-  hash = "sha256-FJFQGKqpDre3Oc5Qsuumug7nyaXLG064akLTktrqwdo=";
+  version = "0.19.0";
+  hash = "sha256-mfMVkSwbO8P/Ch30+SYkKVBo3yL9ZojGNsEc49TRw2E=";
   vendorHash = "sha256-p2B7duASfM2k+6mqcHfomcP6OG7PbXIW/5sA2+D55Uw=";
 }
--- packages/yt-dlp/_version.nix
+++ packages/yt-dlp/_version.nix
@@ -1,5 +1,5 @@
 {
-  version = "master-20240105";
-  rev = "b6951271ac014761c9c317b9cecd5e8e139cfa7c";
-  sha256 = "sha256-qOllCsvRRNPu5Jln0iiFv+BJsFrR/veseEnjo1cOYms=";
+  version = "master-20240128";
+  rev = "cae6e461073fb7c32fd32052a3e6721447c469bc";
+  sha256 = "sha256-bo6rEtVDWsmfFbgO7ZJwNLHjjh3rYQV0KjRvwiDLcuY=";
 }
